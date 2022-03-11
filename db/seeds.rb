6760d8b57de7ebc4ead30b70a3a8ca3a02ff1c92
Eric Einerson <eaeiner.gmail.co>
2022-03-10 22:42:21 -0700

    Changed seeding data a bit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 
 10.times do
-    Category.create(title: Faker::Name.name)
+    Category.create(title: Faker::Tea.variety)
 end
 
 10.times do
-    Item.create(title: Faker::Name.name, quantity:rand(1..5))
+    Item.create(title: Faker::Food.dish, quantity:rand(1..5))
 end
 
-20.times do
-    GroceryList.create(title: Faker::Name.name, category: Category.all.sample, item: Item.all.sample)
+10.times do
+    GroceryList.create(title: Faker::Tea.variety, category: Category.all.sample, item: Item.all.sample)
 end
 
 puts "✅ Done seeding!"
